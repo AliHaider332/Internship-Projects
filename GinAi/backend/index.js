103778141c67ab31e72896351873108c27c0f182
AliHaider332 <bhaialihaider332@gmail.com>
2025-10-08 12:28:50 +0500

    Add @google/genai dependency

--- GinAi/backend/index.js
+++ GinAi/backend/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const { chatRouter } = require('./router/aiChat');
 const cors = require('cors');
-require('dotenv').config();
-
 const app = express();
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
@@ -11,7 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', chatRouter);
 app.use('/', (req, res) => {
-  res.status(404).send('404 - Route Not Found');
+  res.send('404');
 });
 
-module.exports = app; // ✅ Export, no app.listen()
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
